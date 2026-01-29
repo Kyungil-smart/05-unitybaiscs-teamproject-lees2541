abe549965c629192a941bee5aff5ab15a93c6c99
Yuchan Lee <flashedmite45@gmail.com>
2026-01-29 17:30:41 +0900

    [Add] 스킬 애니메이션 추가

--- Assets/Scripts/Boss/BossAnimator.cs
+++ Assets/Scripts/Boss/BossAnimator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Boss.Skills;
 using UnityEngine;
@@ -6,6 +7,8 @@ namespace Boss
 {
 	public class BossAnimator : MonoBehaviour
 	{
+		private static readonly int LaserDirection = Animator.StringToHash("LaserDirection");
+
 		enum SkillState
 		{
 			Started,
@@ -13,11 +16,23 @@ namespace Boss
 			Ended
 		}
 
-		private Dictionary<(BossSkillType, SkillState), int> paramsHashDict = new()
+		private Dictionary<(BossSkillType, SkillState), int> parameterHashDict = new()
 		{
 			{ (BossSkillType.BasicCast, SkillState.Started), Animator.StringToHash("BasicSkillStarted") },
 			{ (BossSkillType.BasicCast, SkillState.Performed), Animator.StringToHash("BasicSkillPerformed") },
 			{ (BossSkillType.BasicCast, SkillState.Ended), Animator.StringToHash("BasicSkillEnded") },
+			{ (BossSkillType.HorizontalLaser, SkillState.Started), Animator.StringToHash("LaserSkillStarted") },
+			{ (BossSkillType.HorizontalLaser, SkillState.Performed), Animator.StringToHash("LaserSkillPerformed") },
+			{ (BossSkillType.HorizontalLaser, SkillState.Ended), Animator.StringToHash("LaserSkillEnded") },
+			{ (BossSkillType.VerticalLaser, SkillState.Started), Animator.StringToHash("LaserSkillStarted") },
+			{ (BossSkillType.VerticalLaser, SkillState.Performed), Animator.StringToHash("LaserSkillPerformed") },
+			{ (BossSkillType.VerticalLaser, SkillState.Ended), Animator.StringToHash("LaserSkillEnded") }
+		};
+
+		private Dictionary<(BossSkillType, SkillState), Action<Animator>> skillActionDict = new()
+		{
+			{ (BossSkillType.HorizontalLaser, SkillState.Started), a => { a.SetBool(LaserDirection, true); } },
+			{ (BossSkillType.VerticalLaser, SkillState.Started), a => { a.SetBool(LaserDirection, false); } }
 		};
 
 		private Animator animator;
@@ -33,8 +48,8 @@ namespace Boss
 
 		private void SetAnimation(BossSkillType skillType, SkillState skillState)
 		{
-			if (!paramsHashDict.TryGetValue((skillType, skillState), out var hash)) return;
-			animator.SetTrigger(hash);
+			if (skillActionDict.TryGetValue((skillType, skillState), out Action<Animator> action)) action(animator);
+			if (parameterHashDict.TryGetValue((skillType, skillState), out var hash)) animator.SetTrigger(hash);
 		}
 	}
 }
