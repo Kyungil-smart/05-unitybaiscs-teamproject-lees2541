9b36103c3e6b76c03134cecb59cd83087bc93720
Yuchan Lee <flashedmite45@gmail.com>
2026-01-30 17:57:39 +0900

    캐릭터 오류 수정 및 Cinemachine 활용 예시 Scene 추가 (#18)
    * [Add] TextMeshPro 에셋 설치
    
    * [Add] [Refactor] 캐릭터와 카메라 예시 Scene 추가 및 리팩토링
    
    캐릭터 리팩토링 (움직임 좌표계 관련 단순화, 점프는 건드리지 않았습니다.
    Interact 간소화 (우선 플레이어 전방의 오브젝트만 시도하게 설정)
    IInteract 인터페이스 기능 추가
    
    카메라 전환 예시 Scene 생성 (Cinemachine 활용)

---
+++ Assets/Scripts/ExampleNamespace/CameraChangeExample.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ExampleNamespace
+{
+	public class CameraChangeExample : MonoBehaviour
+	{
+		[Serializable]
+		public enum CameraState
+		{
+			QuarterWorld,
+			QuarterCamera,
+			TpsCamera,
+			FpsCamera,
+			SideviewWorld
+		}
+
+		public CameraState State
+		{
+			get => state;
+			set
+			{
+				state = value;
+				ChangeState(value);
+			}
+		}
+
+		private CameraState state;
+		public CinemachineVirtualCameraBase FpsCamera;
+		public CinemachineVirtualCameraBase TpsCamera;
+		public CinemachineVirtualCameraBase QuarterViewCamera;
+		public CinemachineVirtualCameraBase SideViewCamera;
+		public UnityChan.PlayerController Player;
+
+		private Camera mainCamera;
+		private List<CinemachineVirtualCameraBase> cameras = new();
+		private string[] stateNames = Enum.GetNames(typeof(CameraState));
+
+		private void Awake()
+		{
+			mainCamera = GetComponent<Camera>();
+
+			cameras.Add(FpsCamera);
+			cameras.Add(TpsCamera);
+			cameras.Add(QuarterViewCamera);
+			cameras.Add(SideViewCamera);
+		}
+
+		private void Start()
+		{
+			ChangeState(CameraState.QuarterCamera);
+		}
+
+		void ChangeState(CameraState state)
+		{
+			foreach (var cameraBase in cameras)
+			{
+				cameraBase.Priority = -1;
+			}
+
+			if (state == CameraState.FpsCamera)
+			{
+				mainCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Player"));
+			}
+			else
+			{
+				mainCamera.cullingMask |= (1 << LayerMask.NameToLayer("Player"));
+			}
+
+			switch (state)
+			{
+				case CameraState.QuarterWorld:
+
+					Player.PerspectiveState = UnityChan.PlayerController.MovementSpace.World;
+					QuarterViewCamera.Priority = 10;
+					break;
+				case CameraState.QuarterCamera:
+					Player.PerspectiveState = UnityChan.PlayerController.MovementSpace.Camera;
+					QuarterViewCamera.Priority = 10;
+					break;
+				case CameraState.TpsCamera:
+					Player.PerspectiveState = UnityChan.PlayerController.MovementSpace.Camera;
+					TpsCamera.Priority = 10;
+					break;
+				case CameraState.FpsCamera:
+					Player.PerspectiveState = UnityChan.PlayerController.MovementSpace.Camera;
+					FpsCamera.Priority = 10;
+					break;
+				case CameraState.SideviewWorld:
+					Player.PerspectiveState = UnityChan.PlayerController.MovementSpace.World;
+					SideViewCamera.Priority = 10;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(state), state, null);
+			}
+		}
+
+		private void OnGUI()
+		{
+			foreach (var s in stateNames)
+			{
+				if (GUILayout.Button(s))
+				{
+					ChangeState((CameraState)Enum.Parse(typeof(CameraState), s));
+				}
+			}
+		}
+	}
+}
